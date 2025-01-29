60faf6bb91b94795f7ab6f702f95df538d566805
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2025-01-29 23:12:20 +0530

    Adds routes

--- src/constants/products.js
+++ src/constants/products.js
@@ -2,6 +2,13 @@ const products = [
   { id: 1, name: "Mobile 1", description: "Description 1", price: "$200" },
   { id: 2, name: "Mobile 2", description: "Description 2", price: "$300" },
   { id: 3, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 4, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 5, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 6, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 7, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 8, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 9, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 10, name: "Mobile 3", description: "Description 3", price: "$400" },
 ];
 
 export default products;
